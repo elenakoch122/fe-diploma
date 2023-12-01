fd54f89c900302b4cf5aa667fb3135e05509c953
Elena Kochegarova <lelet@ngs.ru>
2023-12-01 21:38:58 +0700

    fixed color change at the end of the Stagebar

--- src/components/blocks/StageBar/StageBar.js
+++ src/components/blocks/StageBar/StageBar.js
@@ -2,14 +2,21 @@ import { useSelector } from 'react-redux';
 import Wrapper from '../../Wrapper';
 import Stage from '../../elements/Stage/Stage';
 import style from './StageBar.module.css';
+import { useEffect, useState } from 'react';
 
 export default function StageBar() {
   const stages = useSelector(state => state.stages.stages);
+  const currentStage = useSelector(state => state.stages.current);
+  const [listClasses, setListClasses] = useState(`${style.stageBar__list} ${style.dark}`);
+
+  useEffect(() => {
+    currentStage === 4 ? setListClasses(`${style.stageBar__list} ${style.orange}`) : setListClasses(`${style.stageBar__list} ${style.dark}`);
+  }, [currentStage]);
 
   return (
     <div className={style.stageBar}>
       <Wrapper>
-        <ul className={style.stageBar__list}>
+        <ul className={listClasses}>
           {Object.entries(stages).map(s => {
             return (
               <li className={style.stageBar__item} key={s[0]}>

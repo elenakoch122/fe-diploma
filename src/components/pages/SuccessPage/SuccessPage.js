21678ac26e01830b0746e0bd35ceb9f3481868eb
Elena Kochegarova <lelet@ngs.ru>
2024-03-15 10:11:28 +0700

    added tab name

--- src/components/pages/SuccessPage/SuccessPage.js
+++ src/components/pages/SuccessPage/SuccessPage.js
@@ -2,6 +2,7 @@ import OrderInfo from "../../blocks/OrderInfo/OrderInfo";
 import Thanks from "../../blocks/Thanks/Thanks";
 
 export default function SuccessPage() {
+  document.title = 'Заказ оформлен';
   return (
     <>
       <Thanks />
